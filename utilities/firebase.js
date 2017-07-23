1d7b992a021c0c306600db4276416d6997feb3aa
Rudy Matos <rudymatos.dev@gmail.com>
2017-07-23 12:16:20 -0400

    firebase changes

--- utilities/firebase.js
+++ utilities/firebase.js
@@ -9,18 +9,11 @@ exports.FirebaseHelper = class {
         var nextVersion = 1.0
         ref.orderByChild('version').limitToLast(1).once('value', function (snap) {
             if (snap.val() !== null) {
-                var currentVersion = snap.val()[(Object.keys(snap.val())[0])].version
-                var movieDBVersion = App.movieDBVersion
-                var sum = currentVersion + movieDBVersion
-                console.log( Math.round(sum))
-                console.log( Math.round((sum) * 10))
-                console.log(Math.round((sum) * 10)/10)
-                nextVersion = Math.round((sumb) * 10)/10
-                console.log(`CurrentVersion : ${currentVersion} , DBVersion : ${movieDBVersion} , NextVersion : ${nextVersion}`)
+                nextVersion = snap.val()[(Object.keys(snap.val())[0])].version + 0.1
             }
         }).then(value => {
-            var movieListing = {version: nextVersion, updatedOn: new Date().toString(), data: data}
-            // ref.push(movieListing)
+            var movieListing = { version: nextVersion, updatedOn: new Date().toString(), data: data }
+            ref.push(movieListing)
             response(constants.DATA_SAVED_IN_FIREBASE_SUCESSFULLY)
         });
     }
