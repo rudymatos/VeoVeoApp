75ec50066186a65ddea90f354ce48e652a7913af
Rudy Matos <rudymatos.dev@gmail.com>
2017-07-22 22:34:05 -0400

    creating class for firebase file

--- utilities/firebase.js
+++ utilities/firebase.js
@@ -1,15 +1,11 @@
 exports.FirebaseHelper = class {
-
     constructor(firebase) {
         this.firebase = firebase
     }
-
     saveDataToFirebase(response, data) {
         var ref = firebase.app().database().ref().child('users');
         ref.orderByChild('order').equalTo(9).limitToLast(1).on('child_added', function (currentObject) {
             console.log(currentObject.val())
         })
-
-        // response(ref.toString())
     }
 }
